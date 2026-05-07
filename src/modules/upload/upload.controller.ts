95c111ec2694ecb5127e6310dd27590c6cd9a689
muhammad_refda <muhammad_refda@users.noreply.github.com>
2026-05-07 14:23:07 +0700

    Protect upload endpoint

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -7,13 +7,19 @@ import {
   UseInterceptors,
   ParseFilePipe,
   MaxFileSizeValidator,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 const UPLOAD_MAX_FILE_SIZE_BYTES =
   (Number(process.env.UPLOAD_MAX_FILE_SIZE_MB) || 25) * 1024 * 1024;
 
+@ApiTags('Upload')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
